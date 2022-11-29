ba7e9799caf396c8570a647f88610ae8c4b6c2b7
Mirela Kostova <mir.s.kostova@gmail.com>
2022-11-29 10:57:28 +0200

    Generate table with movies info

---
+++ src/components/movies.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { getMovies } from "../Starter Code/services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  render() {
+    const movies = this.state.movies;
+
+    console.log("type:", movies);
+
+    return (
+      <>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+export default Movies;
