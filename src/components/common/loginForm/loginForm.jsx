e7fc3f20015d074ea5f961f35c526c5549ab4b6f
Mirela Kostova <mir.s.kostova@gmail.com>
2023-02-08 10:57:02 +0200

    joi

--- src/components/common/loginForm/loginForm.jsx
+++ src/components/common/loginForm/loginForm.jsx
@@ -1,13 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Joi from "joi";
 import Illustration from "./media/illustration.svg";
+import { Link } from "react-router-dom";
 import "./loginForm.css";
 
 const LoginForm = () => {
+  const schema = {
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .required(),
+  };
+
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
 
-  const handleSubmit = (e) => {};
+  const validate = (username, password) => {
+    const errors = {};
+
+    if (username.trim() === "") {
+      errors.username = "Username is required.";
+    }
+
+    if (password.trim() === "") {
+      errors.password = "Password is required.";
+    }
+
+    return Object.keys(errors).length === 0 ? null : errors;
+    // const validateUsername = Joi.validate(username, schema);
+
+    // const validatePassword = Joi.validate(password, schema);
+  };
+
+  const handleSubmit = (event) => {
+    if (event) event.preventDefault();
+    console.log("Hello");
+
+    // const errors = validate(username, password);
+
+    // if (errors) return;
+  };
 
   return (
     <div className="wrapper-container d-flex justify-content-center">
@@ -16,20 +48,22 @@ const LoginForm = () => {
           <h1>Welcome back</h1>
           <p>Please enter your details</p>
 
-          {/* Email input */}
+          {/* Username input */}
           <div className="form-outline">
             <input
               autoFocus
-              type="email"
-              id="emailForm"
+              type="username"
+              id="usernameForm"
               value={username}
               className="form-control"
               onChange={(e) => setUsername(e.target.value)}
             />
-            <label className="form-label" htmlFor="emailForm">
-              Email address
+            <label className="form-label" htmlFor="usernameForm">
+              Username
             </label>
           </div>
+          {/* <div className="alert alert-ganger">{validate().username}</div> */}
+          {/* {errors && <div className="alert alert-ganger">{errors}</div>} */}
 
           {/* Password input */}
           <div className="form-outline mb-4">
@@ -44,6 +78,7 @@ const LoginForm = () => {
               Password
             </label>
           </div>
+          {/* <div className="alert alert-ganger">{validate().password}</div> */}
 
           {/* 2 column grid layout for inline styling */}
           <div className="row mb-4">
